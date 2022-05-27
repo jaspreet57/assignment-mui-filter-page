6dfcfda6564e5fa7f3aa0c91b1c40b2d9bf07b20
Jaspreet Singh <erjaspreet57@gmail.com>
2022-05-27 10:32:34 +0530

    Fix: Search input UI in AppNavbar and npm warnings

--- src/components/AppNavBar/AppNavBar.styles.js
+++ src/components/AppNavBar/AppNavBar.styles.js
@@ -1,4 +1,4 @@
-import { styled, alpha } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import InputBase from '@mui/material/OutlinedInput';
 
 export const Search = styled('div')(() => ({
